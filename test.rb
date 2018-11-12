035fab00fd1e0c05caabdfd421dda46187a3f661
Luke Hefson <luke@github.com>
2018-11-12 17:34:04 +0000

    Update and rename test.php to test.rb

--- test.rb
+++ test.rb
@@ -1,11 +1,15 @@
-// https://github.com/QualityAssuranceOrg/cuddly-dollop
+# https://github.com/QualityAssuranceOrg/cuddly-dollop
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in augue at velit posuere lacinia sed vel sem.
 Cras placerat luctus sem vel vulputate.
 
-// #2
+# #2
 
 Aenean elit nisi, auctor a tincidunt in, rhoncus eu erat. Integer a arcu in nibh dictum dictum.
 Nunc hendrerit facilisis porta. Aliquam ante neque, malesuada quis euismod ut, vehicula condimentum felis.
-Suspendisse vitae nibh arcu. Quisque accumsan felis non turpis dictum id pretium arcu malesuada.
+Suspendisse vitae nibh arcu.
+
+# https://github.com/QualityAssuranceOrg/cuddly-dollop/issues/2
+
+Quisque accumsan felis non turpis dictum id pretium arcu malesuada.
 Pellentesque iaculis magna at quam tempor fermentum. Nulla ut urna sit amet tellus tristique
