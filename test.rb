0351e1addaf23c0f9a6eed96698c152b3b6786a6
Luke Hefson <luke@github.com>
2018-11-12 17:34:50 +0000

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,15 +1,15 @@
 # https://github.com/QualityAssuranceOrg/cuddly-dollop
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in augue at velit posuere lacinia sed vel sem.
-Cras placerat luctus sem vel vulputate.
+Cras placerat luctus sem vel https://github.com/QualityAssuranceOrg/cuddly-dollop vulputate.
 
 # #2
 
 Aenean elit nisi, auctor a tincidunt in, rhoncus eu erat. Integer a arcu in nibh dictum dictum.
 Nunc hendrerit facilisis porta. Aliquam ante neque, malesuada quis euismod ut, vehicula condimentum felis.
-Suspendisse vitae nibh arcu.
+Suspendisse vitae nibh #2 arcu.
 
 # https://github.com/QualityAssuranceOrg/cuddly-dollop/issues/2
 
 Quisque accumsan felis non turpis dictum id pretium arcu malesuada.
-Pellentesque iaculis magna at quam tempor fermentum. Nulla ut urna sit amet tellus tristique
+Pellentesque iaculis magna at quam tempor fermentum. Nulla ut urna sit amet tellus https://github.com/QualityAssuranceOrg/cuddly-dollop/issues/2 tristique
